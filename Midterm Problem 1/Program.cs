ad2e23a59532513fcccef435d18cc8d7285fdc24
Gary Hembree <gbhembree22@amarillocollege.com>
2024-09-09 13:10:40 -0500

    Add project files.

---
+++ Midterm Problem 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_Problem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompts users to input their numbers
+            Console.WriteLine("Input number 1");
+            int num1 = Convert.ToInt32(Console.ReadLine()); // converts the input to int
+            Console.WriteLine("Input number 2");
+            int num2 = Convert.ToInt32(Console.ReadLine()); // converts the input to int
+            Console.WriteLine("Inpute number 3");
+            int num3 = Convert.ToInt32(Console.ReadLine()); //converts the input to int
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("The biggest number is " + num1);
+                }
+            }
+            if (num2 > num1)
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine("The biggest number is " + num2);
+                }
+            }
+            if (num3 > num1)
+            {
+                if (num3 > num2)
+                {
+                    Console.WriteLine("The biggest number is " + num3);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
